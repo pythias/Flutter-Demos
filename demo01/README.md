e74a916034061d2f9c02e70dc23aa2333a9a48f2
chenjie5 <chenjie5@staff.weibo.com>
2017-09-25 11:40:23 +0800

    demo01: Hello World

---
+++ demo01/README.md
@@ -0,0 +1,8 @@
+# demo01
+
+A "Hello, world!" Flutter project.
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](http://flutter.io/).
---
+++ demo01/lib/main.dart
@@ -0,0 +1,1 @@
+import 'package:flutter/material.dart';void main() {  runApp(      new Center(          child: new Text(              'Hello, world!',              textDirection: TextDirection.ltr          )      )  );}
